d29e2b2cdc8d798dc6d76e6d89e6e4138ec67df1
Usstzst <37054930+Usstzst@users.noreply.github.com>
2019-04-06 20:43:52 +0800

    Update ex2_reg.m

--- machine-learning-ex2/ex2/ex2_reg.m
+++ machine-learning-ex2/ex2/ex2_reg.m
@@ -109,6 +109,7 @@ initial_theta = zeros(size(X, 2), 1);
 % Set regularization parameter lambda to 1 (you should vary this)
 lambda = 1;
 
+
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 
